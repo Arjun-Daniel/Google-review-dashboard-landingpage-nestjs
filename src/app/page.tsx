1bcc29dcb5f0e7eec59db06a9d911c7f3ea6f1ca
Arjun Daniel <130634616+Arjun-Daniel@users.noreply.github.com>
2024-10-10 17:02:22 +0530

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -29,7 +29,7 @@ export default function Home() {
         title="Learn how to fullfil your needs"
       >
         This section is to highlight a promo or demo video of your product.
-        Analysts says a landing page with video has 3% more conversion rate. So,
+        Analysts says a landing page with video has 4% more conversion rate. So,
         don&apos;t forget to add one. Just like this.
       </SectionTitle>
 
